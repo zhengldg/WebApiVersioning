756397eb56fb2714d1d5ebd75bb945de9505c587
zhengledong <zhengledong@cedarhd.com>
2020-07-02 14:59:31 +0800

    新增 Webapi 结合 razor 示例

---
+++ WebApiTest/Global.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTest
+{
+    public class Global
+    {
+        public const string Name = "Webapi with razor";
+    }
+}
